c7fbc83b6fc43a6c645aab2923ed699e0a667b82
elijah571 <elijahfx43@gmail.com>
2025-03-19 19:47:37 +0100

    package.jsonn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     proxy: {
-      "/api/": {
-        target: "http://localhost:3000",
+      '/api/': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        secure: false, 
+        secure: false,
       },
     },
   },
