2655525d3c5eed9ccfb19667e352cad4fa10a2b2
elijah571 <elijahfx43@gmail.com>
2025-03-19 16:06:18 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import { app } from "./app.js";
+import express from 'express'
+import path from 'path';
 
 const port = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
 
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 // Start server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
