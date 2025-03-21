c87537d5854090c1799c3ac931e739d2860a7428
elijah571 <elijahfx43@gmail.com>
2025-03-21 13:46:40 +0100

    built for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Export Vite config with defineConfig function
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',  // Ensure build files go to the parent directory's dist folder
+    outDir: '../dist', // Ensure build files go to the parent directory's dist folder
   },
   server: {
     host: '0.0.0.0',
     proxy: {
       '/api/': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000', // Proxy requests to your backend
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
