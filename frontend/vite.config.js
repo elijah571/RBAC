a2e04660c2080ecfa35924cb313fdf88b25bf802
elijah571 <elijahfx43@gmail.com>
2025-03-21 11:14:19 +0100

    deployment setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     host: '0.0.0.0',
     proxy: {
@@ -13,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
