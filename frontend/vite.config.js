66c59eb34d09d7f25220ef9ec7e00dc6a268e6ee
elijah571 <elijahfx43@gmail.com>
2025-03-21 11:39:14 +0100

    deploymnt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-  
   server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/api/': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // Set the development server to run on port 3000
+    host: 'localhost',  // Optional, allows access from other devices on the network
+    port: 3000,         // Set port to 3000
+    strictPort: true,   // If the port is already in use, it will fail
   },
-})
+};
