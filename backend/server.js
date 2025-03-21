25dad1e55f7fe04ecadfb23de2c1fb7b256bfda3
elijah571 <elijahfx43@gmail.com>
2025-03-21 13:59:03 +0100

    built for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import path from 'path';
-import { app } from './app.js'; // Import the app instance from backend/app.js
+import { app } from './app.js'; 
 
 const port = process.env.PORT || 3000;
-const __dirname = path.resolve(); // Get the absolute path
+const __dirname = path.resolve();
 
-// Serve static files from React build (dist folder)
+// Serve static files
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-// Handle all other routes by sending the React index.html
-app.get('*', (req, res) => {
+// Handle all routes by serving the index.html
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
